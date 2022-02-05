e2b67769917bf3700fbe465529ff6021f4745c92
Nicole Saunders <nicostar26@yahoo.com>
2022-02-05 16:22:46 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.addFunds')
+const items = document.querySelectorAll('.funds')
+
+button.addEventListener('click', () => {
+    
+    for(i=0; i < items.length; i++){
+        console.log(items)
+    }
+})
